69eebd5c7ee76a1cf42cc382ce11ab76ea6045b3
ghazalfallahpour <ghazalfa@icloud.com>
2022-11-27 15:27:00 -0800

    test 2 passes

--- src/main/java/timedelayqueue/TimeDelayQueue.java
+++ src/main/java/timedelayqueue/TimeDelayQueue.java
@@ -1,8 +1,10 @@
 package timedelayqueue;
 
+import java.sql.Timestamp;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.UUID;
+import java.util.*;
 
 // TODO: write a description for this class
 // TODO: complete all methods, irrespective of whether there is an explicit TODO or not
@@ -10,6 +12,19 @@ import java.util.UUID;
 // TODO: State the rep invariant and abstraction function
 // TODO: what is the thread safety argument?
 public class TimeDelayQueue {
+
+    // Store all current messages in a list
+    private List<PubSubMessage> messages;
+
+    // Store the total number of messages added (irrespective of those that have been removed)
+    long totalMessageCount = 0;
+
+    // Store the delay of the TimeDelayQueue (initialized in constructor)
+    int delay;
+
+    // Store all operations that have occurred by storing the timestamp in a list
+    // Assume that multiple operations cannot happen at the same millisecond
+    List<Long> history;
 
     // a comparator to sort messages
     private class PubSubMessageComparator implements Comparator<PubSubMessage> {
@@ -23,12 +38,27 @@ public class TimeDelayQueue {
      * @param delay the delay, in milliseconds, that the queue can tolerate, >= 0
      */
     public TimeDelayQueue(int delay) {
+        this.delay = delay;
+        this.messages = new ArrayList<>();
+        this.history = new ArrayList<>();
+    }
+
+    private synchronized void addToHistory() {
+        history.add(System.currentTimeMillis());
     }
 
     // add a message to the TimeDelayQueue
     // if a message with the same id exists then
     // return false
-    public boolean add(PubSubMessage msg) {
+    public synchronized boolean add(PubSubMessage msg) {
+        addToHistory();
+
+        if (!messages.contains(msg)) {
+            messages.add(msg);
+            Collections.sort(messages, new PubSubMessageComparator());
+            totalMessageCount++;
+            return true;
+        }
         return false;
     }
 
@@ -38,12 +68,20 @@ public class TimeDelayQueue {
      * @return
      */
     public long getTotalMsgCount() {
-        return -1;
+        return this.totalMessageCount;
     }
 
     // return the next message and PubSubMessage.NO_MSG
     // if there is ni suitable message
-    public PubSubMessage getNext() {
+    public synchronized PubSubMessage getNext() {
+        addToHistory();
+        Timestamp currentTimestamp = new Timestamp(System.currentTimeMillis());
+
+        PubSubMessage nextMsg = messages.get(0);
+        if (currentTimestamp.getTime() - nextMsg.getTimestamp().getTime() >= delay) {
+            messages.remove(nextMsg);
+            return nextMsg;
+        }
         return PubSubMessage.NO_MSG;
     }
 
@@ -51,8 +89,28 @@ public class TimeDelayQueue {
     // performed on this TimeDelayQueue over
     // any window of length timeWindow
     // the operations of interest are add and getNext
-    public int getPeakLoad(int timeWindow) {
-        return -1;
+    public synchronized int getPeakLoad(int timeWindow) {
+        int temp = 0;
+        int count = 0;
+        long timestamp;
+        int highest = 0;
+
+        for(int i = 0; i<history.size(); i++){
+            count = 0;
+            timestamp = history.get(i) +timeWindow;
+            temp = i;
+            while(temp< history.size()&& history.get(temp)<=timestamp){
+                    count++;
+                    temp++;
+            }
+
+            if(count>highest){
+                highest = count;
+            }
+        }
+
+
+        return highest;
     }
 
 }
